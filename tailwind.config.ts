3381d3da22e6d99bf859490dc3c43e053d215356
htameez <htameez@smu.edu>
2025-07-26 15:04:00 -0500

    created constants

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+    content: [
+        './app/**/*.{ts,tsx}', // Make sure app directory is scanned
+        './components/**/*.{ts,tsx}', // If you have components later
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                heading: ['DidotBoldItalic', 'sans-serif'],
+                body: ['BirkaSemiBold', 'sans-serif'],
+            },
+            colors: {
+                text: '#454525',
+                background: {
+                    DEFAULT: '#b6b992',
+                    alt1: '#9a9d6c',
+                    alt2: '#c5ca91',
+                    alt3: '#a8ab7a',
+                },
+            },
+        },
+    },
+    plugins: [],
+};
+
+export default config;
